202af00b1652e3d38c2bdc15b05e14bdffd271ce
AijaMartinez <168228355+AijaMartinez@users.noreply.github.com>
2024-09-19 21:24:20 -0500

    clase recolecta

--- POO/recolecta.dart
+++ POO/recolecta.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Colaborador {
   String? _nombreCompleto;
   int? _tipoColaborador;
@@ -14,4 +16,52 @@ class Colaborador {
       '{"Nombre": "$_nombreCompleto",  "Aporte": "$_aporte","Tipo": "$_tipoColaborador"}';
 
   double getAporte() => _aporte;
+}
+
+class Recolecta {
+  List<Colaborador> _colaboradores = [];
+  double? _montoRecaudar;
+  double? _balance = 0;
+
+  Recolecta(this._montoRecaudar, [this._balance]);
+
+  addColaborador(Colaborador colaborador) {
+    _colaboradores.add(colaborador);
+    _balance = _balance! + colaborador.getAporte();
+  }
+
+  bool finalizada() => _balance! > _montoRecaudar!;
+
+  List<Colaborador> generosos() {
+    List<Colaborador> generososList = [];
+    for (var colaborador in _colaboradores) {
+      if (colaborador.getAporte() >= 10000) {
+        generososList.add(colaborador);
+      }
+    }
+
+    return generososList;
+  }
+
+  double recaudoGenerosos() {
+    double total = 0;
+
+    for (var dinero in generosos()) {
+      total += dinero.getAporte();
+    }
+
+    return total;
+  }
+
+  double promedioGenerosos() => _balance! / _montoRecaudar!;
+
+  double recaudoPorTipo(tipo) {
+    double recaudo = 0;
+    for (var recaTipo in _colaboradores) {
+      if (recaTipo.getTipo() == tipo) {
+        recaudo += recaTipo.getAporte();
+      }
+    }
+    return recaudo;
+  }
 }
