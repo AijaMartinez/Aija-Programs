0b2187feecfd7bb4f5ae7143ffa09c5bed876121
AijaMartinez <168228355+AijaMartinez@users.noreply.github.com>
2024-10-13 22:58:55 -0500

    add

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'pages/homescr.dart';
 
 void main() {
-  runApp(MyProgram());
+  runApp(const MyProgram());
 }
 
 class MyProgram extends StatelessWidget {
+  const MyProgram({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Datos del Usuario',
+      debugShowCheckedModeBanner: false,
       home: HomeSca(),
     );
   }
--- flutter_app/lib/pages/homescr.dart
+++ flutter_app/lib/pages/homescr.dart
@@ -19,18 +19,23 @@ class _HomeScaState extends State<HomeSca> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
+        length: 1,
         child: Scaffold(
+          backgroundColor: const Color.fromARGB(113, 158, 158, 158),
           appBar: AppBar(
-              title: Text(
+            title: const Center(
+              child: Text(
                 'Datos del usuario',
-                style: TextStyle(color: Colors.white),
+                style: TextStyle(color: Color.fromARGB(255, 191, 152, 236)),
               ),
-              backgroundColor: Colors.black),
-          bottomNavigationBar: TabBar(tabs: [
-            Tab(text: 'Informacion 1'),
-            Tab(text: 'Informacion 2'),
-          ]),
+            ),
+            backgroundColor: Colors.black,
+            bottom: const TabBar(tabs: [
+              Tab(
+                icon: Icon(Icons.add_home),
+              ),
+            ]),
+          ),
           body: TabBarView(children: [
             _detallesDelUsuario(),
           ]),
@@ -39,7 +44,7 @@ class _HomeScaState extends State<HomeSca> {
 
   Widget _detallesDelUsuario() {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(160),
       child: SingleChildScrollView(
         child: Center(
           key: _formKey,
@@ -48,13 +53,15 @@ class _HomeScaState extends State<HomeSca> {
             child: Column(
               children: [
                 TextFormField(
+                  cursorColor: const Color.fromARGB(255, 221, 189, 10),
                   controller: _controller,
                   decoration: const InputDecoration(
+                    floatingLabelStyle: TextStyle(color: Colors.blue),
+                    labelStyle: TextStyle(color: Color.fromARGB(255, 95, 35, 136)),
                     border: OutlineInputBorder(),
                     labelText: 'ID',
                   ),
-                  keyboardType: TextInputType
-                      .number, 
+                  keyboardType: TextInputType.number,
                 ),
                 const Padding(padding: EdgeInsets.all(10)),
                 ElevatedButton(
@@ -72,32 +79,63 @@ class _HomeScaState extends State<HomeSca> {
                                 builder: (context, snapshot) {
                                   if (snapshot.connectionState ==
                                       ConnectionState.waiting) {
-                                    return Center(
-                                        child: CircularProgressIndicator());
+                                    return const Center(
+                                      child: CircularProgressIndicator(
+                                        strokeWidth: 10,
+                                        color: Colors.black,
+                                      ),
+                                    );
                                   } else if (id > 0) {
-                                    return AlertDialog(
+                                    return Theme(
+                                      data: Theme.of(context).copyWith(
+                                        textTheme: TextTheme(
+                                          bodyMedium: TextStyle(color: const Color.fromARGB(255, 223, 214, 231)),
+                                        ),
+                                      ) , 
+                                      child: AlertDialog(
+                                      backgroundColor: Colors.black,
                                       content: snapshot.data != null
-                                          ? Column(
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                Text('ID: ${actividad!.id}'),
-                                                Text(
-                                                    'Nombre: ${actividad.name}'),
-                                                Text(
-                                                    'Username: ${actividad.username}'),
-                                                Text(
-                                                    'Email: ${actividad.email}'),
-                                                Text(
-                                                    'Teléfono: ${actividad.phone}'),
-                                                Text(
-                                                    'Website: ${actividad.website}'),
-                                                Text(
-                                                    'Dirección: ${actividad.address.street}, ${actividad.address.city}'),
-                                                Text(
-                                                    'Compañía: ${actividad.company.name}'),
-                                              ],
+                                          ? Padding(
+                                              padding: const EdgeInsets.all(60),
+                                              child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  Text('ID: ${actividad!.id}'),
+                                                  Text(
+                                                      'Name: ${actividad.name}'),
+                                                  Text(
+                                                      'Username: ${actividad.username}'),
+                                                  Text(
+                                                      'Email: ${actividad.email}'),
+                                                  Text(
+                                                      'Address: ${actividad.address.street}'),
+                                                  Text(
+                                                      'Suite: ${actividad.address.suite}'),
+                                                  Text(
+                                                      'City: ${actividad.address.city}'),
+                                                  Text(
+                                                      'ZipCode: ${actividad.address.zipcode}'),
+                                                  const Text('Address-geo: '),
+                                                  Text(
+                                                      'lat: ${actividad.address.geo.lat}'),
+                                                  Text(
+                                                      'lng: ${actividad.address.geo.lng}'),
+                                                  Text(
+                                                      'Phone: ${actividad.phone}'),
+                                                  Text(
+                                                      'Website: ${actividad.website}'),
+                                                  Text(
+                                                      'Company: ${actividad.company.name}'),
+                                                  const Text(
+                                                      'Company-catchPhrase:'),
+                                                  Text(actividad
+                                                      .company.catchPhrase),
+                                                  const Text('Company-bs:'),
+                                                  Text(actividad.company.bs),
+                                                ],
+                                              ),
                                             )
-                                          : Text(
+                                          : const Text(
                                               'No se encontraron datos para el ID proporcionado.'),
                                       actions: [
                                         TextButton(
@@ -107,17 +145,19 @@ class _HomeScaState extends State<HomeSca> {
                                           child: const Text('Cerrar'),
                                         ),
                                       ],
-                                    );
+                                    ));
+                                    
                                   }
-                                  return Text('NO FOUND');
+                                  return const Text('NO FOUND');
                                 },
                               ));
                     } else {
                       showDialog(
                         context: _formKey.currentContext!,
                         builder: (context) => AlertDialog(
-                          title: Text('Error'),
-                          content: Text('Por favor, introduce un ID válido.'),
+                          title: const Text('Error'),
+                          content:
+                              const Text('Por favor, introduce un ID válido.'),
                           actions: [
                             TextButton(
                               onPressed: () {
